a337e819898e0c59f40ee841ae51122c316a36c1
Paras <parasthind299@gmail.com>
2021-10-27 16:03:48 +0530

    Web app initialised

---
+++ src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  <AddTask v-show="showAddTask" @add-task="addTask" />
+  <Tasks
+    @toggle-reminder="toggleReminder"
+    @delete-task="deleteTask"
+    :tasks="tasks"
+  />
+</template>
+
+<script>
+import Tasks from "./../components/Tasks.vue";
+import AddTask from "./../components/AddTask.vue";
+export default {
+  name: "Home",
+  props: {
+    showAddTask: Boolean,
+  },
+  components: {
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    async addTask(task) {
+      const res = await fetch("http://localhost:5000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
+    },
+
+    async deleteTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: "DELETE",
+      });
+
+      res.status === 200
+        ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+        : alert("Error deleting task!");
+    },
+
+    async toggleReminder(id) {
+      const taskToggle = await this.fetchTask(id);
+      const updateTask = { ...taskToggle, reminder: !taskToggle.reminder };
+
+      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updateTask),
+      });
+
+      const data = await res.json();
+
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      );
+    },
+    async fetchTasks() {
+      const res = await fetch("http://localhost:5000/tasks");
+
+      const data = await res.json();
+      return data;
+    },
+
+    async fetchTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`);
+
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    this.tasks = await this.fetchTasks();
+  },
+};
+</script>
